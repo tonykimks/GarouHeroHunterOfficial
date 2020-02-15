33cdfbf8d4eeb0baebf4de4d1f696d35cd9cdb9d
Anthony Kim <anthonykim02@berkeley.edu>
2020-02-15 15:27:29 -0800

    improved running, grass. adding transition to battle

--- Assets/main/PlayerMovement.cs
+++ Assets/main/PlayerMovement.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -8,8 +9,16 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 4f;
     public Rigidbody2D rb;
     public Animator animator;
+    public bool running = false;
+
+    public GameObject battleStartPanel;
 
     Vector2 movement;
+
+    private void Start()
+    {
+        battleStartPanel.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 0f);
+    }
 
     // Update is called once per frame
     void Update()
@@ -21,35 +30,51 @@ public class PlayerMovement : MonoBehaviour
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.sqrMagnitude);
+        animator.SetBool("Running", running);
 
-        if (Input.GetKeyDown(KeyCode.X))
+        if (Input.GetKey(KeyCode.X))
         {
             moveSpeed = 7f;
+            running = true;
         }
 
         if (Input.GetKeyUp(KeyCode.X))
         {
             moveSpeed = 4f;
+            running = false;
         }
 
-        if (Input.GetKeyDown(KeyCode.DownArrow))
+        if (Input.GetKey(KeyCode.DownArrow))
         {
             animator.SetInteger("Direction", 0);
         }
 
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
             animator.SetInteger("Direction", 1);
         }
 
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.UpArrow))
         {
             animator.SetInteger("Direction", 2);
         }
 
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.RightArrow))
         {
             animator.SetInteger("Direction", 3);
+        }
+
+        if (movement.x < 0.001 && movement.x > -0.001 && movement.y < 0.001 && movement.y > -0.001)
+        {
+            running = false;
+        }
+
+
+
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            StartCoroutine(startBattle());
         }
     }
 
@@ -57,5 +82,25 @@ public class PlayerMovement : MonoBehaviour
     {
         // Movement
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    IEnumerator startBattle()
+    {
+        battleStartPanel.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 1f);
+        yield return new WaitForSeconds(0.2f);
+        battleStartPanel.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 0f);
+        yield return new WaitForSeconds(0.2f);
+        battleStartPanel.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 1f);
+        yield return new WaitForSeconds(0.2f);
+        battleStartPanel.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 0f);
+        yield return new WaitForSeconds(0.2f);
+        battleStartPanel.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 1f);
+        yield return new WaitForSeconds(0.2f);
+        battleStartPanel.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 0f);
+        yield return new WaitForSeconds(0.2f);
+        battleStartPanel.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 1f);
+        yield return new WaitForSeconds(0.5f);
+        SceneManager.LoadScene("GarouHeroHunter");
+        yield return 0;
     }
 }
